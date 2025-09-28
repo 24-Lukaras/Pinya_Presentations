9e27f9cca107aa7ca94aecef0eb679b512bc886b
24-Lukaras <85632668+24-Lukaras@users.noreply.github.com>
2025-09-28 16:02:50 +0200

    refac: changed method in chat api hub

--- ChatApi/ChatHub.cs
+++ ChatApi/ChatHub.cs
@@ -12,31 +12,31 @@ public class ChatHub : Hub
     public async Task Login(string username)
     {
         _users[Context.ConnectionId] = username;
-        await Clients.AllExcept(Context.ConnectionId).SendAsync(MESSAGE_RECEIVED_EVENT, $"{username} has connected");
+        await Clients.Others.SendAsync(MESSAGE_RECEIVED_EVENT, $"{username} has connected");
     }
 
     public async Task JoinLobby(string lobbyName)
     {
         await Groups.AddToGroupAsync(Context.ConnectionId, lobbyName);
         _userGroups[Context.ConnectionId] = lobbyName;
-        await Clients.GroupExcept(lobbyName, Context.ConnectionId).SendAsync(MESSAGE_RECEIVED_EVENT, $"{_users[Context.ConnectionId]} has connected to lobby {lobbyName}");
+        await Clients.OthersInGroup(lobbyName).SendAsync(MESSAGE_RECEIVED_EVENT, $"{_users[Context.ConnectionId]} has connected to lobby {lobbyName}");
     }
 
     public async Task SendMessage(string message)
     {
         if (_userGroups.TryGetValue(Context.ConnectionId, out string? lobbyName))
         {
-            await Clients.GroupExcept(lobbyName, Context.ConnectionId).SendAsync(MESSAGE_RECEIVED_EVENT, $"{_users[Context.ConnectionId]} says: {message}");
+            await Clients.OthersInGroup(lobbyName).SendAsync(MESSAGE_RECEIVED_EVENT, $"{_users[Context.ConnectionId]} says: {message}");
         }
         else
         {
-            await Clients.AllExcept(Context.ConnectionId).SendAsync(MESSAGE_RECEIVED_EVENT, $"{_users[Context.ConnectionId]} says: {message}");
+            await Clients.Others.SendAsync(MESSAGE_RECEIVED_EVENT, $"{_users[Context.ConnectionId]} says: {message}");
         }
     }
 
     public override async Task OnDisconnectedAsync(Exception? exception)
     {
-        await Clients.AllExcept(Context.ConnectionId).SendAsync(MESSAGE_RECEIVED_EVENT, $"{_users[Context.ConnectionId]} has disconnected");
+        await Clients.Others.SendAsync(MESSAGE_RECEIVED_EVENT, $"{_users[Context.ConnectionId]} has disconnected");
         _users.Remove(Context.ConnectionId, out _);
         await base.OnDisconnectedAsync(exception);
     }
