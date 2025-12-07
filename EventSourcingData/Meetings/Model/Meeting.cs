adf222c514cfbea66dd4902be53a07d8974abd03
24-Lukaras <85632668+24-Lukaras@users.noreply.github.com>
2025-12-07 17:30:21 +0100

    fix: event sourcing - changed collections to internals

--- EventSourcingData/Meetings/Model/Meeting.cs
+++ EventSourcingData/Meetings/Model/Meeting.cs
@@ -13,8 +13,8 @@ public class Meeting
     public IEnumerable<MeetingNote> Notes => NotesInternal;
     public IEnumerable<MeetingEvent> Events => EventsInternal;
 
-    public ICollection<MeetingNote> NotesInternal { get; private set; } = new HashSet<MeetingNote>();
-    public ICollection<MeetingEvent> EventsInternal { get; private set; } = new HashSet<MeetingEvent>();
+    internal ICollection<MeetingNote> NotesInternal { get; private set; } = new HashSet<MeetingNote>();
+    internal ICollection<MeetingEvent> EventsInternal { get; private set; } = new HashSet<MeetingEvent>();
 
 
     private Meeting(Guid id, string title, string description, MeetingStatus status)
