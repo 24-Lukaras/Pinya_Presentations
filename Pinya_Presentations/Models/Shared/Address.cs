391c708e4aa0bf8354e48a1c9b72d015e4201cfd
24-Lukaras <85632668+24-Lukaras@users.noreply.github.com>
2026-02-22 18:10:11 +0100

    feat: strongly typed frontend - typed model complex property

---
+++ Pinya_Presentations/Models/Shared/Address.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pinya_Presentations.Models;
+
+public class Address
+{
+    [Display(Name = "Stát")]
+    public string? Country { get; init; }
+
+    [Display(Name = "Město")]
+    public string? City { get; init; }
+
+    [Display(Name = "PSČ")]
+    public string? ZipCode { get; init; }
+
+    [Display(Name = "Ulice")]
+    public string? Street { get; init; }
+}
--- Pinya_Presentations/Models/UserModel.cs
+++ Pinya_Presentations/Models/UserModel.cs
@@ -1,8 +1,16 @@
-﻿namespace Pinya_Presentations.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pinya_Presentations.Models;
 
 public class UserModel
 {
     public string Username { get; init; } = string.Empty;
     public Toggle Active { get; init; } = new Toggle();
     public Checkbox IsAdmin { get; init; } = new Checkbox();
+
+    [Display(Name = "Adresa")]
+    public Address Address { get; init; } = new Address();
+
+    [Display(Name = "Kontaktní adresa")]
+    public Address ContactAddress { get; init; } = new Address();
 }
